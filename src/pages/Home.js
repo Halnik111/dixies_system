2b0a1e63fd46c6971f3e22106057ba2771d7816d
halnik111 <halonikolas@gmail.com>
2024-08-31 16:10:42 +0200

    socket

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,34 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import './Home.css';
 import {AuthContext} from "../context/AuthContext";
 import NoAccess from "../components/NoAccess";
+import {SocketContext} from "../context/SocketContext";
 
 const Home = () => {
     const {currentUser} = useContext(AuthContext);
+    const { socket } = useContext(SocketContext);
+
+    useEffect( () => {
+        socket?.on('testEmit', data => {
+            console.log("socket working!");
+        });
+
+    }, [socket]);
+
+    const test = () => {
+        console.log('clicked')
+        socket.emit('test', "test success?");
+    }
 
     return (
         <div className={'home'}>
             {currentUser ? (
-                <div>Ordering System</div>
+                <div>
+                    Ordering System
+                    <div onClick={() => test()}>
+                        button
+                    </div>
+                </div>
             ) : (
                 <NoAccess />
             )}
