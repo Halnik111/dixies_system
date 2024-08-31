7d87435e56256c41ac9cc2d776415cf37dcda847
halnik111 <halonikolas@gmail.com>
2024-08-31 18:20:13 +0200

    socket

--- src/apiReq.js
+++ src/apiReq.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiReq = axios.create({
-    baseURL: "https://dixiessystembackend-production.up.railway.app/",
+    baseURL: "https://dixiessystembackend-production.up.railway.app",
     // baseURL: "http://localhost:8080",
     withCredentials: true,
 });
--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -7,7 +7,9 @@ export const SocketContextProvider = ({children}) => {
     const [socket, setSocket] = useState(null);
 
     useEffect(() => {
-        setSocket(io(process.env.SOCKET_URL));
+        setSocket(io('wss://dixiessystembackend-production.up.railway.app/8080'));
+
+        // setSocket(io('ws://localhost:8080'));
     },[]);
 
     return (
